f714f1300184ba63313c00b0a1deda79899bd7c1
Git-Lukas <132563585+Git-Lukas@users.noreply.github.com>
2023-06-01 17:54:27 +0200

    agregamos main y style

---
+++ Color-picker/main.js
@@ -0,0 +1,14 @@
+const defaultColor = '#0000ff'
+const chooseInput = document.querySelector('.choose-input')
+const viewBtn = document.querySelector('.view-btn')
+const colorText = document.querySelector('.color-text')
+const colorDiv = document.querySelector('.view-color')
+
+
+colorDiv.style.backgroundColor = defaultColor
+chooseInput.value = defaultColor
+
+viewBtn.addEventListener('click', () => {
+    colorText.textContent = chooseInput.value
+    colorDiv.style.backgroundColor = chooseInput.value
+})
